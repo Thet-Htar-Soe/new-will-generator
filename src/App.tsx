c2a959fc52c483c80d384830c04048c6f4d815ca
ThetHtarSoe <105904414+Thet-Htar-Soe@users.noreply.github.com>
2025-07-16 08:53:34 +0800

    delete some files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import ClientPage from "./pages/ClientPage";
-import SideBar from "./pages/Sidebar";
 
 function App() {
   return (
     <>
       <div className="flex">
-        {/* <SideBar /> */}
         <ClientPage />
       </div>
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./pages/Navbar";
-import AnalysisPage from "./pages/AnalysisPage";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -12,8 +11,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <NavBar />
       <Routes>
         <Route path="/" element={<App />} />
-        {/* <Route path="/analysis" element={<App />} /> */}
-        {/* <Route path="/analysis" element={<AnalysisPage />} /> */}
       </Routes>
     </BrowserRouter>
   </StrictMode>
